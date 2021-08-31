98177d77f58dfd0b1f7782f9c7d035f7dc479f46
ManoharMahanthi <manoharmahanthi008@gmail.com>
2021-08-31 09:11:07 +0530

    Tweets by UserId

--- index.js
+++ index.js
@@ -35,80 +35,82 @@ client.on("messageCreate", (msg) => {
     // const endpointURL = `https://api.twitter.com/2/tweets/search/recent?query=${QUERY}`;
     // const endpointURL =
     // "https://api.twitter.com/2/tweets/search/recent?query=%2523caturday%2520has%253Aimages%2520-is%253Aretweet";
-    const endpointURL = "https://api.twitter.com/2/tweets/search/recent?query=";
-    async function getRequest() {
-      // These are the parameters for the API request
-      // specify Tweet IDs to fetch, and any additional fields that are required
-      // by default, only the Tweet ID and text are returned
-      const params = {
-        // ids: "1423830326665650179", // Edit Tweet IDs to look up
-        query: "musk",
-        "tweet.fields": "lang,author_id", // Edit optional query parameters here
-        "user.fields": "created_at", // Edit optional query parameters here
-      };
+    
 
-      // this is the HTTP header that adds bearer token authentication
-      const res = await needle("get", endpointURL, params, {
-        headers: {
-          "User-Agent": "v2TweetLookupJS",
-          authorization: `Bearer ${token}`,
-        },
-      });
+    const userId = "70931004";
+const url = `https://api.twitter.com/2/users/${userId}/tweets`;
 
-      if (res.body) {
-        return res.body;
-      } else {
-        throw new Error("Unsuccessful request");
-      }
+// The code below sets the bearer token from your environment variables
+// To set environment variables on macOS or Linux, run the export command below from the terminal:
+// export BEARER_TOKEN='YOUR-TOKEN'
+const bearerToken = process.env.BEARER_TOKEN;
+
+const getUserTweets = async () => {
+    let userTweets = [];
+
+    // we request the author_id expansion so that we can print out the user name later
+    let params = {
+        "max_results": 100,
+        "tweet.fields": "created_at",
+        "expansions": "author_id"
     }
-    var data;
-    (async () => {
-      try {
-        // Make request
-        const response = await getRequest();
-        if (response) {
-          console.log(response);
-          i = 0;
-          while (response.data[i].lang != "en") {
-            i++;
-          }
-          data = response.data[i].text;
 
-          // console.log(window.btoa(message));
-          msg.reply(data);
-          console.log(data);
+    const options = {
+        headers: {
+            "User-Agent": "v2UserTweetsJS",
+            "authorization": `Bearer ${bearerToken}`
         }
-      } catch (e) {
-        console.log(e);
-        process.exit(-1);
-      }
-      // process.exit();
-    })();
+    }
 
-    // msg.reply(data);
+    let hasNextPage = true;
+    let nextToken = null;
+    let userName;
+    console.log("Retrieving Tweets...");
 
-    // var axios = require("axios");
-    // var API_key = "60334492ddbf059c9d3e63f3f8c788c3";
-    // var city = message;
-    // var config = {
-    //   method: "get",
-    //   // url: "https://api.twitter.com/TwitterDev/status/1228393702244134912",
-    //   url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=60334492ddbf059c9d3e63f3f8c788c3`,
-    //   headers: {},
-    // };
-    // var weather = "cool";
-    // axios(config)
-    //   .then(function (response) {
-    //     weather = response.data.weather[0].description;
-    //     console.log(weather);
-    //     if (weather) {
-    //       msg.reply(weather);
-    //     }
-    //   })
-    //   .catch(function (error) {
-    //     // console.log(error);
-    //   });
-  }
-});
+    while (hasNextPage) {
+        let resp = await getPage(params, options, nextToken);
+        if (resp && resp.meta && resp.meta.result_count && resp.meta.result_count > 0) {
+            userName = resp.includes.users[0].username;
+            if (resp.data) {
+                userTweets.push.apply(userTweets, resp.data);
+            }
+            if (resp.meta.next_token) {
+                nextToken = resp.meta.next_token;
+            } else {
+                hasNextPage = false;
+            }
+        } else {
+            hasNextPage = false;
+        }
+    }
+
+    console.dir(userTweets, {
+        depth: null
+    });
+    console.log(`Got ${userTweets.length} Tweets from ${userName} (user ID ${userId})!`);
+
+}
+
+const getPage = async (params, options, nextToken) => {
+    if (nextToken) {
+        params.pagination_token = nextToken;
+    }
+
+    try {
+        const resp = await needle('get', url, params, options);
+
+        if (resp.statusCode != 200) {
+            console.log(`${resp.statusCode} ${resp.statusMessage}:\n${resp.body}`);
+            return;
+        }
+        return resp.body;
+    } catch (err) {
+        throw new Error(`Request failed: ${err}`);
+    }
+}
+
+getUserTweets();
+}
+  });
 
 client.login(process.env.TOKEN);
